1da6ddd5777817947dc77d1ec4079312c662a472
DCardigo <123099651+DCardigo@users.noreply.github.com>
2023-06-21 11:01:24 +0000

    practica finalizada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+	const [rojo, setRojo] = useState("")
+	const [amarillo, setAmarillo] = useState("")
+	const [verde, setVerde] = useState("")
+
+	function cambiarRojo(){
+		setRojo("selected")
+		if (rojo === "selected"){
+			setRojo (" ")
+		}
+	}
+
+	function cambiarAmarillo(){
+		setAmarillo("selected")
+		if (amarillo === "selected"){
+			setAmarillo (" ")
+		}
+	}
+
+
+	function cambiarVerde(){
+		setVerde("selected")
+		if (verde === "selected"){
+			setVerde (" ")
+		}
+	}
+
+	return(
+		<div className="fondo bg-dark d-flex flex-column mt-5 p-3">
+			
+			<button type="button" onClick={cambiarRojo} className={"btn btn-danger rounded-circle  mb-2 " + rojo}></button>
+			
+			
+			<button type="button" onClick={cambiarAmarillo} className={"btn btn-warning rounded-circle mb-2 " + amarillo}></button>
+			
+			
+			<button type="button" onClick={cambiarVerde} className={"btn btn-success rounded-circle mb-2  " + verde}></button>
+			
+
 		</div>
-	);
-};
+	)
+}
+
 
 export default Home;
