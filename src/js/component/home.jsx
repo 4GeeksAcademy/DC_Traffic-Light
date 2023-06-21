03e8ea5d65d4809bd5428dcbc65453495ec86a28
DCardigo <123099651+DCardigo@users.noreply.github.com>
2023-06-21 15:06:59 +0000

    falta limpiar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,46 +7,141 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 
+
+	// ESTADOS
+
 	const [rojo, setRojo] = useState("")
 	const [amarillo, setAmarillo] = useState("")
 	const [verde, setVerde] = useState("")
+	const [morado, setMorado] = useState("")
+	const [esVisible, setEsVisible] = useState(false);
 
-	function cambiarRojo(){
-		setRojo("selected")
-		if (rojo === "selected"){
-			setRojo (" ")
+		// ROJO
+
+		function cambiarRojo(){
+			setRojo("selected")
+			if (rojo === "selected"){
+				setRojo (" ")	
+			}
+			if (amarillo === "selected"){
+				setAmarillo (" ")
+			}
+			if (verde === "selected"){
+				setVerde (" ")
+			}
+			if (morado === "selected"){
+				setMorado (" ")
+			}
+		};
+
+		// AMARILLO
+
+		function cambiarAmarillo(){
+			setAmarillo("selected")
+			if (amarillo === "selected"){
+				setAmarillo (" ")
+			}
+			if (verde === "selected"){
+				setVerde (" ")
+			}
+			if (rojo === "selected"){
+				setRojo (" ")	
+			}
+			if (morado === "selected"){
+				setMorado (" ")
+			}
+		};
+
+		// VERDE
+
+
+		function cambiarVerde(){
+			setVerde("selected")
+			if (verde === "selected"){
+				setVerde (" ")
+			}
+			if (rojo === "selected"){
+				setRojo (" ")	
+			}
+			if (amarillo === "selected"){
+				setAmarillo (" ")
+			}
+			if (morado === "selected"){
+				setMorado (" ")
+			}
+		};
+
+		// RANDOM
+
+		function colorRandom  () {
+
+			let colores = ["Rojo","Amarillo","Verde"]
+
+			let resultado = colores[Math.floor(Math.random() * colores.length)];
+
+			resultado === "Rojo" ? cambiarRojo() : " ";
+			resultado === "Amarillo" ? cambiarAmarillo() : " ";
+			resultado === "Verde" ? cambiarVerde() : " ";
+
+			}
+
+		// MORADO
+
+		function cambiarMorado(){
+			setMorado("selected")
+			if (morado === "selected"){
+				setMorado (" ")
+			}
+			if (rojo === "selected"){
+				setRojo (" ")	
+			}
+			if (amarillo === "selected"){
+				setAmarillo (" ")
+			}
+			if (verde === "selected"){
+				setVerde (" ")
+			}
 		}
-	}
-
-	function cambiarAmarillo(){
-		setAmarillo("selected")
-		if (amarillo === "selected"){
-			setAmarillo (" ")
-		}
-	}
 
 
-	function cambiarVerde(){
-		setVerde("selected")
-		if (verde === "selected"){
-			setVerde (" ")
-		}
-	}
+			// NEW COLOR
+			function newColor () {
+				setEsVisible(true);
+				esVisible === true ? setEsVisible(false) : setEsVisible(true);
+
+			  };
+
+				
+
+
+
 
 	return(
-		<div className="fondo bg-dark d-flex flex-column mt-5 p-3">
+	
+		<>
+			<div className="top" ></div>
 			
-			<button type="button" onClick={cambiarRojo} className={"btn btn-danger rounded-circle  mb-2 " + rojo}></button>
-			
-			
-			<button type="button" onClick={cambiarAmarillo} className={"btn btn-warning rounded-circle mb-2 " + amarillo}></button>
-			
-			
-			<button type="button" onClick={cambiarVerde} className={"btn btn-success rounded-circle mb-2  " + verde}></button>
-			
+			<div className="fondo bg-dark d-flex flex-column  p-3 ">
+				
+				<button type="button" onClick={cambiarRojo} className={"btn btn-danger rounded-circle  mb-2 " + rojo}></button>
+				
+				
+				<button type="button" onClick={cambiarAmarillo} className={"btn btn-warning rounded-circle mb-2 " + amarillo}></button>
+				
+				
+				{esVisible && <button type="button" onClick={cambiarMorado} className={"morado btn rounded-circle mb-2  " + morado}></button>}
 
-		</div>
+
+				<button type="button" onClick={cambiarVerde} className={"btn btn-success rounded-circle mb-2  " + verde}></button>
+				
+
+			</div>
+			<button type="button" onClick={colorRandom} className="random btn btn-info ">Random</button>
+			<button type="button" onClick={newColor} className="random btn btn-info ">new color</button>
+		</>
 	)
+		
+		
 }
 
 
